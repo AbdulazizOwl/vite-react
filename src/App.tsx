fd6f3f9cfdf8cddcb811d7e5a4c751d95d89cb59
Abdulaziz Alqahtani <124428369+AbdulazizOwl@users.noreply.github.com>
2025-02-19 17:34:09 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import ApplePayPage from "./ApplePayPage";
 
 function App() {
-    return (
-        <div className="App">
-            <ApplePayPage />
-        </div>
-    );
+  return (
+    <div className="App">
+      <h1>Vite + React</h1>
+      <p>Edit src/App.tsx and save to test HMR</p>
+    </div>
+  );
 }
 
 export default App;
