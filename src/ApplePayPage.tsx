411ba844ab1ab472f8e1506e369dc5cddef36d8d
Abdulaziz Alqahtani <124428369+AbdulazizOwl@users.noreply.github.com>
2025-02-19 17:23:09 -0500

    Update ApplePayPage.tsx

--- src/ApplePayPage.tsx
+++ src/ApplePayPage.tsx
@@ -1,1 +1,23 @@
+import React from "react";
 
+const ApplePayPage: React.FC = () => {
+    return (
+        <div style={{ padding: "20px", textAlign: "center" }}>
+            <h1>Apple Pay Integration</h1>
+            <button
+                id="apple-pay-button"
+                style={{
+                    appearance: "-apple-pay-button",
+                    "-apple-pay-button-style": "black",
+                    width: "200px",
+                    height: "50px",
+                }}
+                onClick={() => alert("Apple Pay Button Clicked")}
+            >
+                Buy with Apple Pay
+            </button>
+        </div>
+    );
+};
+
+export default ApplePayPage;
